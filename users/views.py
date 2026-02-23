49d82e22150c529ee054ab70ed98cfa7ca2d31d4
Nemwel-Boniface <nemwelnyandoro20@gmail.com>
2026-02-23 22:54:29 +0300

    Add custom message for when a user logs out

--- users/views.py
+++ users/views.py
@@ -90,4 +90,7 @@ class LogoutView(GenericAPIView):
         serializer = self.serializer_class(data=request.data)
         serializer.is_valid(raise_exception=True)
         serializer.save()
-        return Response(status=status.HTTP_204_NO_CONTENT)
+        return Response(
+            {"message": "Logout successful"},
+            status=status.HTTP_200_OK,
+        )
