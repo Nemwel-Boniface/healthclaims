c395dd157dee52b19c263b974c478e78c0ec042b
Nemwel-Boniface <nemwelnyandoro20@gmail.com>
2026-03-12 17:44:28 +0300

    Add more seed data to make the porcess of testing our adjudication engine smoother

--- claims/management/commands/seed_data.py
+++ claims/management/commands/seed_data.py
@@ -2,11 +2,7 @@ from django.core.management.base import BaseCommand
 from claims.models import Member, Provider, Procedure
 
 class Command(BaseCommand):
-    """
-    Custom Django Management Command to provide a 'Ready-to-Test' environment.
-    Usage: python manage.py seed_data
-    """
-    help = 'Seeds initial data for Ginja AI Claims Demo'
+    help = 'Seeds diverse scenarios for Ginja AI Claims Demo Adjudication'
 
     def handle(self, *args, **kwargs):
         self.stdout.write("Cleaning existing records...")
@@ -14,26 +10,23 @@ class Command(BaseCommand):
         Provider.objects.all().delete()
         Procedure.objects.all().delete()
 
-        # Create M123 - The Standard Test Member
-        Member.objects.create(
-            member_id="M123", 
-            first_name="Nemwel", 
-            last_name="Dev", 
-            benefit_balance=40000.00,
-            is_active=True
-        )
+        # 1. THE STANDARD (A claim that has a Full Approval)
+        Member.objects.create(member_id="M123", first_name="Nemwel", last_name="Active", benefit_balance=50000.00, is_active=True)
 
-        # Create H456 - The Standard Test Provider
-        Provider.objects.create(
-            provider_code="H456", 
-            name="Aga Khan University Hospital"
-        )
+        # 2. THE BROKE (A claim with Zero Balance Rejection)
+        Member.objects.create(member_id="M777", first_name="John", last_name="Empty", benefit_balance=0.00, is_active=True)
 
-        # Create P001 - The Standard Test Procedure (Avg Cost: 5k)
-        Procedure.objects.create(
-            code="P001", 
-            name="Dental Surgery", 
-            average_cost=5000.00
-        )
+        # 3. THE INACTIVE (A claim with Eligibility Rejection)
+        Member.objects.create(member_id="M999", first_name="Sarah", last_name="Expired", benefit_balance=100000.00, is_active=False)
 
-        self.stdout.write(self.style.SUCCESS('Successfully seeded Ginja Health Claims data!'))
+        # 4. THE LOW LIMIT (A claim with Partial Approval/Capping)
+        Member.objects.create(member_id="M555", first_name="Alice", last_name="Capped", benefit_balance=2000.00, is_active=True)
+
+        # PROVIDER
+        Provider.objects.create(provider_code="H456", name="Aga Khan University Hospital")
+
+        # PROCEDURES
+        Procedure.objects.create(code="P001", name="Dental Surgery", average_cost=5000.00) # Threshold for fraud: 10k
+        Procedure.objects.create(code="P002", name="Consultation", average_cost=1500.00)
+
+        self.stdout.write(self.style.SUCCESS('Successfully seeded all Adjudication Scenarios!'))
